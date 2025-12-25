019dbf0c2e8354c7304612ceebc247d4249abf44
YING12332 <xiaojiejieshuo250@126.com>
2025-12-25 23:32:03 +0800

    完成了版本控制以及下载安装包功能

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,21 @@
-# ===== Build stage =====
+# ===== builder =====
 FROM golang:1.25-alpine AS builder
-
 WORKDIR /app
 
-# 先拷依赖文件，提高缓存命中
-COPY go.mod go.sum ./
-RUN go mod download
+# 可选：如果你项目里用了 CGO（一般没有），需要 gcc/musl-dev
+# RUN apk add --no-cache build-base
 
-# 再拷源码
 COPY . .
+RUN go build -mod=vendor -o server ./cmd/server
 
-# 构建静态二进制
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server ./cmd/server
-
-# ===== Runtime stage =====
+# ===== runtime =====
 FROM alpine:3.20
-
 WORKDIR /app
+
+# 证书（如果后面要 https/oss/minio 会用到）
+RUN apk add --no-cache ca-certificates && update-ca-certificates
+
 COPY --from=builder /app/server /app/server
 
 EXPOSE 8080
-CMD ["/app/server"]
+ENTRYPOINT ["/app/server"]
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
+	"github.com/YING12332/OtakuMaster-server/internal/config"
+	httpx "github.com/YING12332/OtakuMaster-server/internal/http"
+	"github.com/YING12332/OtakuMaster-server/internal/modules/version"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config.Load()
+
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
 	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		c.JSON(200, gin.H{"status": "ok"})
 	})
 
-	_ = r.Run(":8080")
+	// 组装版本模块
+	versionRepo := &version.FileRepo{BaseDir: config.Cfg.VersionDataDir}
+	versionSvc := version.NewService(versionRepo)
+	versionHandler := version.NewHandler(versionSvc)
+
+	downloadHandler := httpx.NewDownloadHandler(config.Cfg.ApkDir)
+
+	httpx.RegisterRoutes(r, httpx.RouterDeps{
+		VersionHandler:  versionHandler,
+		DownloadHandler: downloadHandler,
+	})
+
+	addr := fmt.Sprintf(":%s", config.Cfg.HTTPPort)
+	_ = r.Run(addr)
 }
